4db52d1bd1e84c82468d68e1e3fa5618f3ba88f6
Mahdi Karimi <mhdksp566@gmail.com>
2024-11-17 00:52:27 +0330

    final edit

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
+function dataFetch() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-
-xhr.onreadystatechange = function() {
-  if (xhr.readyState !== 4){
-    return xhr.readyState;
-  }
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        return xhr.readyState;
+      }
 
 
-  if (xhr.status === 200){
-    console.log(JSON.parse(xhr.responseText));
-  } else{
-    console.log("erorr is:", xhr.status, xhr.statusText);
-  }
-};
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject(`Error is : ${xhr.status} , ${xhr.statusText}`);
+        }
+      }
+    ;
 
-xhr.send();
+    xhr.send();
+  });
+}
+
+
+
+
+dataFetch()
+  .then((data) => {console.log("Users:", data);
+  }).catch((error) => {console.error(error);});
